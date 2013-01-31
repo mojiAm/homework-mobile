b1f82a9fd7f36ee751d348898bf55690745956fe
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-31 14:33:03 +0330

    add practice

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>App</title>
+	<link rel="stylesheet" href="./style.css" />
+</head>
+<body>
+	<div class="contain">
+		<!-- Header -->
+		<header>
+			<div class="header">
+				<div class="logo"></div>
+			</div>
+			<div class="pic-text">
+				<img class="header-pic" src="./images/generic_background.png" alt="" />
+				<div class="text">Simple, secure sharing from anywhere</div>
+			</div>
+		</header>
+		<!-- Content -->
+		<section class="content">
+			<div class="menu">
+				<nav>
+					<menu>
+						<li><a class="login" href="#"><p>Login</p></a></li>
+						<li><a class="sign" href="#"><p>Sign Up</p></a></li>
+						<li><a class="box" href="#"><p>Box Mobile App</p></a></li>
+						<li><a class="about" href="#"><p>About</p></a></li>
+					</menu>
+				</nav>
+			</div>
+			<div class="detail">
+				<h2>Box for Mobile</h2>
+				<ul>
+					<li class="mobile"><p>Get 5GB FREE and browse your files on the go</p></li>
+					<li class="upload"><p>Upload files from your device to Box</p></li>
+					<li class="share"><p>Share your files with anyone from anywhere</p></li>
+				</ul>
+			</div>
+		</section>
+		<!-- Footer -->
+		<footer>
+			<div class="menu">
+				<nav>
+					<menu>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Video</a></li>
+						<li><a href="#">View Full Site</a></li>
+					</menu>
+					<div class="clear"></div>
+				</nav>
+			</div>
+			<div class="copy">
+				<a href="https://m.box.com/" target="_blank"><p>style: https://m.box.com/</p></a>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
